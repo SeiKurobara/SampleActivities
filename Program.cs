de20f190992dfb7b04b6d83c2fab09c0e8c32322
SeiKurobara <seijinkurobara@gmail.com>
2023-11-26 02:07:34 +0800

    sample

--- Program.cs
+++ Program.cs
@@ -1,14 +1,36 @@
-﻿using static SampleActivities.SampleActivitiesInner.sampleClass;
+﻿using System.Runtime.CompilerServices;
+using static SampleActivities.SampleActivitiesInner.sampleClass;
 using static System.Console;
 
 namespace SampleActivities;
 
 internal class Program
 {
+    // Namespaces
+    // Classes
     static void Main(string[] args)
     {
         WriteLine("Hello, World!");
         sampleMethod();
 
     }
+
+
+
+    
+}
+    
+class Sample
+{
+    // Fields
+    //Fields allow the following modifiers:
+    //Static modifier static
+    //Access modifiers public internal private protected
+    //Inheritance modifier new
+    //Unsafe code modifier unsafe
+    //Read-only modifier readonly
+    //Threading modifier volatile
+
+    string name = "Melanio";
+    int age = 23;
 }
